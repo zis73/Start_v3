63efedb657eb25b987008a14df5db5f4788f6e91
Eldar Akavov <desert73eagle@gmail.com>
2019-09-27 13:10:34 +0300

    end lab_12 add lab_11

---
+++ src/lab_11/lab_11.java
@@ -0,0 +1,10 @@
+package lab_11;
+
+public class lab_11 {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 6, 8, 2, 3, 4, 7};
+        for (int i = 0; i < arr.length; i++) {
+            
+        }
+    }
+}
--- src/lab_12/Main.java
+++ src/lab_12/Main.java
@@ -1,20 +1,36 @@
 package lab_12;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 5, 6, 7, 8, 9, 3, 4, 10};
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] arr =new int[n];
+        RandomInit(arr,n);
+        //int[] arr = {1, 5, 6, 8, 9, 10, 2, 4};
         for (int i = 0; i < arr.length; i++) {
             int least = i;
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[j] < arr[least]) {
                     least = j;
-                } else {
-                    int res = arr[i];
-                    arr[i] = arr[least];
-                    arr[least] = res;
                 }
-                System.out.println(arr[i]);
             }
+            int res = arr[i];
+            arr[i] = arr[least];
+            arr[least] = res;
+
+            System.out.println(arr[i]);
+
+        }
+
+    }
+
+    public static void RandomInit(int[] arr, int n) {
+        Random rand = new Random();
+        for (int i = 0; i < n; i++) {
+            arr[i] = rand.nextInt(100);
         }
     }
 }
