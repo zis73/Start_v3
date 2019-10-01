ec26bd56cf007e169ff35491b08234542ffbab16
Eldar Akavov <desert73eagle@gmail.com>
2019-10-01 11:47:56 +0300

    create lab_13 Polymorphism

---
+++ src/lab_13/Main.java
@@ -0,0 +1,11 @@
+package lab_13;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Обычный треугольник:");
+        Triangle.doIt();
+        System.out.println("------------");
+        System.out.println("Прямоугольный треугольник:");
+        rightTriangle.Exception();
+    }
+}
---
+++ src/lab_13/Triangle.java
@@ -0,0 +1,40 @@
+package lab_13;
+
+import java.util.Scanner;
+
+public class Triangle {
+
+    public static void main(String[] args) {
+        doIt();
+    }
+
+    public static void doIt() {
+        Perimetr(3, 4, 1);
+        Square(3, 5, 4);
+    }
+
+    public static void Perimetr(double a, double b, double c) {
+        double P;
+        P = a + b + c;
+        System.out.println("Периметр: " + P);
+
+    }
+
+    public static void Square(double a, double b, double c) {
+        double S;
+        double p = (a + b + c) / 2;
+        S = Math.sqrt((p * (p - a) * (p - b) * (p - c)));
+        System.out.println("Площадь: " + S);
+    }
+}
+//    public static void Vvod() {
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Введите длину a: ");
+//        int a = scan.nextInt();
+//        System.out.println("Введите длину b: ");
+//        int b = scan.nextInt();
+//        System.out.println("Введите длину c: ");
+//        int c = scan.nextInt();
+//        Perimetr(a, b, c);
+//        Square(a, b, c);
+//    }
