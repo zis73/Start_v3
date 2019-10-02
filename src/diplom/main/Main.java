475515a248386f222e4af329bacbb3d1da9fd948
Eldar Akavov <desert73eagle>
2019-10-02 13:20:04 +0300

    need to do Diplom and lab_13

---
+++ src/diplom/main/Main.java
@@ -0,0 +1,22 @@
+package diplom.main;
+
+import java.io.BufferedWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    NewProduct();
+    }
+    public static void NewProduct(){
+        String name;
+        BufferedWriter scan = new BufferedWriter();
+        name = scan.toString();
+        System.out.println(name);
+    }
+    public static void Purchase(){
+
+    }
+    public static void SaleReport(){
+
+    }
+}
--- src/lab_13/rightTriangle.java
+++ src/lab_13/rightTriangle.java
@@ -1,16 +1,17 @@
 package lab_13;
 
 public class rightTriangle extends Triangle {
-    public static void main(String[] args) {
-        Exception();
-    }
 
     public static void Exception() {
-        double a = 4;
-        double b = 5;
-        double c = 1;
+        //double a = 4;
+        //double b = 5;
+       // double c = 1;
         if ((int) Math.pow(c, 2) != (int) Math.pow(a, 2) + (int) Math.pow(b, 2)) {
             System.out.println("Не верно, квадрат гипотенузы не равен сумме квадратов катетов ");
+        }
+
+        public static void Square(double a, double b) {
+
         }
     }
 //    public static void Vivod(){
