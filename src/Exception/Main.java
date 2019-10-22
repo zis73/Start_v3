7937f665b084894b1d118be37ff7c0ba452c4278
Eldar Akavov <desert73eagle@gmail.com>
2019-10-22 10:35:04 +0300

    doing Exception

--- src/Exception/Main.java
+++ src/Exception/Main.java
@@ -1,8 +1,25 @@
 package Exception;
 
 public class Main {
+
+    static int neededSize = 4;
+
     public static void main(String[] args) {
-        MyException exception = new MyException("3");
-        MyException("");
+        String[][] arr = {{"1", "2", "3"}, {"1", "2", "3"}, {"1", "2", "3"}, {"1", "2", "3"}};
+        System.out.println("Checking array size...");
+
+        try {
+            checkArraySize(arr);
+        } catch (MyException e) {
+            System.out.println("Array size incorrect");
+            return;
+        }
+        System.out.println("Checking complete, it's okay");
+    }
+
+    public static void checkArraySize(String[][] arr) throws MyException {
+        if (arr.length != neededSize) {
+            throw new MyException(arr.length, neededSize);
+        }
     }
 }
--- src/Exception/MyException.java
+++ src/Exception/MyException.java
@@ -1,9 +1,20 @@
 package Exception;
 
-public class MyException extends Exception{
-    String msg;
+public class MyException extends Exception {
+    private int length, neededLength;
 
-    public MyException(String msg){
-        super(msg);
+
+    public MyException(int length, int neededLength) {
+        super("Введена не верная длина массива" + neededLength + ", а вы ввели:" + length);
+        this.length = length;
+        this.neededLength = neededLength;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getNeededLength() {
+        return neededLength;
     }
 }
